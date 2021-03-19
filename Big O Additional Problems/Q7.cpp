9a82764adeb87ac64346a2998fd0ddb7b6af03f9
Guilherme Morone Araujo <guimorone@gmail.com>
2021-03-19 18:44:23 -0300

    Q7, Q8 added to Big O

---
+++ Big O Additional Problems/Q7.cpp
@@ -0,0 +1,2 @@
+// Not balanced binary search tree
+// In the worst case => O(n) where "n" is the number of nodes in the tree
---
+++ Big O Additional Problems/Q8.cpp
@@ -0,0 +1,3 @@
+// Searching in a binary tree, but the tree is not a binary search tree
+// Without any property on the nodes, we might have to search through all the nodes
+// O(n), where "n" is the number of nodes in the tree
