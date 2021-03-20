b3294d3d641e6836f86f3fd57d0e1911b073249a
Guilherme Morone Araujo <guimorone@gmail.com>
2021-03-20 17:50:04 -0300

    Q5 done and added to Array and Lists

--- Coding Questions/Array and Lists/Q5 - One Away.cpp
+++ Coding Questions/Array and Lists/Q5 - One Away.cpp
@@ -11,6 +11,25 @@ int main() {
     return 0;
 }
 
+/*                                      SOLUTION 1
+
+    Time Complexity => O(n) => Where "n" is either the length of the "a" string
+    or the length of the "b" string. Since the maximum difference allowed on both
+    is 1, abs(length(a) - length(b)) <= 1 => Roughly, length(a) == length(b). Therefore,
+    as the for loop in the function iterates through the string twice, the time complexity 
+    is O(n + n), i.e, O(n).
+
+    Space Complexity => O(1) => No additional data structures.
+
+    Resolution => As insert and remove actions are similar, we can divide the function into two.
+    In case of replacement, length(a) must be equals to length(b), and both of them must be
+    different only in one place. So, if we find more than one difference, they are not one edit
+    away.
+    Otherwise, in case of insertion or removal, (length(a) - length(b)) must be equal to 1. As
+    with replacement, both of them must be different only in one place. So, if we find more
+    than one difference, they are not one edit away.
+*/
+
 bool oneAway(string a, string b) {
     if(a == b) return true;
     int lenA = a.size(), lenB = b.size();
@@ -51,4 +70,36 @@ bool oneAway(string a, string b) {
     
 
     return count <= 1;
+}
+
+/*                                      SOLUTION 2
+
+    The same of solution 1, but we are not dividing the function here.
+*/
+
+bool oneAway(string a, string b) {
+    if(abs(a.size() - b.size()) > 1) return false;
+
+    // Get shorter and longer string
+    string s1 = a.size() < b.size() ? a : b;
+    string s2 = a.size() < b.size() ? b : a;
+
+    int index1 = 0, index2 = 0;
+    bool foundDifference = false;
+
+    while(index2 < s2.size() && index1 < s1.size()) {
+        if(s1[index1] != s2[index2]) {
+            // Ensure that this is the first difference found
+            if(foundDifference) return false;
+            foundDifference = true;
+
+            // On replace, move shorter pointer
+            if(s1.size() == s2.size()) index1++;
+        } else {
+            index1++; // If matching, move shorter pointer
+        }
+        index2++; // Always move pointer for longer string
+    }
+
+    return true;
 }
